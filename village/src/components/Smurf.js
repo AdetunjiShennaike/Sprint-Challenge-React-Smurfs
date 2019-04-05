dd04dbe8dcef4189d1d0f8f2af27164248c40f15
Adetunji <ashennaike@gmail.com>
2019-04-05 15:02:44 -0400

    realized i should jsut pass in the object as a object and not use push, had a bit of a brain fart. it works now

--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,34 +1,32 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom'
 
 const Smurf = props => {
   console.log(props)
-  let smurf = []
+  let smurf = {}
 
   props.smurfs.map( event => {
    if(`${event.id}` === props.match.params.id) {
-      smurf.push(event)
+      smurf = event
     } 
   })
-  console.log(smurf, smurf.0.name)
+  console.log(smurf, smurf.name)
   
   return (
-    !props.smurfs != 1 
+    !props.name 
   ? 
-    <div className="Smurf">
-      <h3>{smurf.name}</h3>
-      <strong>{smurf.height} tall</strong>
-      <p>{smurf.age} smurf years old</p>
-    </div>
-  :
-    <Link to={`/smurf/${props.id}`}>
       <div className="Smurf">
-        <h3>{props.name}</h3>
-        <strong>{props.height} tall</strong>
-        <p>{props.age} smurf years old</p>
+        <h3>{smurf.name}</h3>
+        <strong>{smurf.height} tall</strong>
+        <p>{smurf.age} smurf years old</p>
       </div>
-    </Link>
+    
+  :
+    <div className="Smurf">
+      <h3>{props.name}</h3>
+      <strong>{props.height} tall</strong>
+      <p>{props.age} smurf years old</p>
+    </div>
   );
 };
 
